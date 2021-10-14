d47105579ad048db3c4435bf62e9de247d5211e0
John Peters <petersjohn0109@gmail.com>
2021-10-13 23:48:14 -0400

    Updated the index links, added authentication

---
+++ ElevenNote.Models/NoteCreate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteCreate
+    {
+        [Required]
+        [MinLength(2,ErrorMessage ="Please enter at least 2 characters.")]
+        [MaxLength(100, ErrorMessage = "This field can only contain a maximum of 1000 characters.")]
+        public string Title { get; set; }
+        [MaxLength(8000)]
+        public string Content { get; set; }
+    }
+}
---
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -0,0 +1,40 @@
+﻿using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.WebMVC.Controllers
+{
+    public class NoteController : Controller
+    {
+        [Authorize]
+        // GET: Note
+        public ActionResult Index()
+        {
+            var model = new NoteListItem[0];
+            return View(model);
+        }
+
+        //Get 
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View();
+        }
+
+
+    }
+
+}
